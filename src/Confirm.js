1c35f6ceb529adddbaaa386791807ebec7ba1f3a
intheblackworld <intheblackworld@gmail.com>
2017-02-18 21:12:01 +0800

    test

--- src/Confirm.js
+++ src/Confirm.js
@@ -10,13 +10,14 @@ export default class Confirm extends React.Component {
   }
   sendConfirmPost () {
 
-    fetch('http://nodejs-express-practice2.herokuapp.com/newapi/members', {
+    fetch('http://nodejs-express-practice2.herokuapp.com/newapi/members/confirm', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-
+        email: this.props.location.query.email,
+        password: this.props.location.query.password
       })
     })
   }
@@ -25,7 +26,7 @@ export default class Confirm extends React.Component {
     return (
       <div>
         <h1>歡迎光臨</h1>
-        <h1>你的ID: {this.props.location.query.id}</h1>
+        <h1>你的email: {this.props.location.query.email}</h1>
         <h1>你的密碼: {this.props.location.query.password}</h1>
         <button
           type="submit"
